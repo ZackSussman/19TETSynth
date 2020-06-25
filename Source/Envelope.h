0d4138ffdab8b7667567eeb6ee5dad0f273bed68
ZackSussman <65458956+ZackSussman@users.noreply.github.com>
2020-06-25 17:46:41 -0400

    working Envelope class!!!

--- Source/Envelope.h
+++ Source/Envelope.h
@@ -9,3 +9,87 @@
 */
 
 #pragma once
+
+
+enum whichStageOfPlayBack {attack, decay, sustain, release, off};
+
+class Envelope {
+
+public:
+    double attackTime = .1;
+    double relativeAttackLevel = 1;
+    
+    double decayTime = .05;
+    double relativeDecayLevel = .8;
+
+    double sustainTime = 1;
+    
+    double releaseTime = .1;
+    double relativeReleaseLevel = 0;
+    
+    double time = 0;
+    whichStageOfPlayBack stage = off;
+
+public:
+    
+    Envelope() {};
+    
+    Envelope(double attack, double attackLevel, double decay, double decayLevel, double sustain, double release, double releaseLevel) {
+        attackTime = attack;
+        relativeAttackLevel = attackLevel;
+        
+        decayTime = decay;
+        relativeDecayLevel = decayLevel;
+        
+        sustainTime = sustain;
+        
+        releaseTime = release;
+        relativeReleaseLevel = releaseLevel;
+        
+    }
+    
+    double preform(double input){
+        if (stage == attack) {
+            return input*relativeAttackLevel*(time/attackTime);
+        }
+        else if (stage == decay) {
+            return input*relativeAttackLevel - (relativeAttackLevel - relativeDecayLevel)*input*((time - attackTime)/(decayTime)) ;
+        }
+        else if (stage == sustain) {
+            return input*relativeDecayLevel;
+        }
+        else if (stage == release) {
+            return input*relativeDecayLevel - (relativeDecayLevel - relativeReleaseLevel)*input*((time - decayTime - attackTime - sustainTime)/(releaseTime)) ;
+        }
+        else {
+            return 0;
+        }
+        
+    }
+    
+    void beginningOfNote() {
+        time = 0;
+        
+    }
+    
+    void update() {
+        time += .01;
+        if (time <= attackTime) {
+            stage = attack;
+        }
+        else if (time <= attackTime + decayTime) {
+            stage = decay;
+        }
+        else if (time <= attackTime + decayTime + sustainTime) {
+            stage = sustain;
+        }
+        else if (time <= attackTime + decayTime + sustainTime + releaseTime) {
+            stage = release;
+        }
+        else {
+            stage = off;
+        }
+    }
+    
+    
+};
--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -13,6 +13,7 @@
 #include <cmath>
 #include "SynthSound.h"
 #include "PluginProcessor.h"
+#include "Envelope.h"
 
 class SynthVoice : public SynthesiserVoice {
 
@@ -26,6 +27,7 @@ public:
     {
         level = velocity;
         frequency = convertMidiNumberToFrequencyModeOne(midiNoteNumber);
+        envelope->beginningOfNote();
     }
     
     void stopNote (float velocity, bool allowTailOff) {
@@ -46,13 +48,14 @@ public:
         double sampleRate = getSampleRate();
         
         for (int sample = 0; sample < numSamples; ++sample) {
-            double signal = level*(double)sinf(frequency*2*M_PI*(sampleNumber/sampleRate));
+            double signal = envelope->preform(level*(double)sinf(frequency*2*M_PI*(sampleNumber/sampleRate)));
             for (int channel = 0; channel  < outputBuffer.getNumChannels(); ++channel) {
                 outputBuffer.addSample(channel, startSample, signal);
                 //outputBuffer.addSample(channel, startSample, signal);
             }
             sampleNumber++;
             ++startSample;
+            envelope->update();
         }
         
     }
@@ -61,6 +64,7 @@ private:
     double level = 0.5;
     double frequency = 440;
     int sampleNumber = 0;
+    Envelope* envelope = new Envelope(.1, 1, .1, .8, 10, .1, 0);
     
 
     
